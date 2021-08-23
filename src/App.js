c4b81e5c1b431a260f2995a634ef043a2de724cb
Parth Patil <parthnpatil06@gmail .com>
2021-08-23 17:05:15 +0200

    Complete blog code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const users = [
+	{
+		username: "john",
+		age: 15,
+		hobbies: ["Football", "Cricket"],
+	},
+	{
+		username: "jane",
+		age: 23,
+		hobbies: ["Reading", "Writing"],
+	},
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [subdomain, setSubDomain] = useState(null);
+	useEffect(() => {
+		const host = window.location.host; // gets the full domain of the app
+
+		const arr = host
+			.split(".")
+			.slice(0, host.includes("localhost") ? -1 : -2);
+		if (arr.length > 0) setSubDomain(arr[0]);
+	}, []);
+	const requestedUser = users.find((user) => user.username === subdomain);
+	return (
+		<div className="app">
+			{subdomain ? (
+				requestedUser ? (
+					<div>
+						<h1>Username</h1>
+						<h3>{requestedUser.username}</h3>
+						<h1>Age</h1>
+						<h3>{requestedUser.age}</h3>
+						<h1>Hobbies</h1>
+						<ul>
+							{requestedUser.hobbies.map((hobby) => (
+								<li key={hobby}>{hobby}</li>
+							))}
+						</ul>
+					</div>
+				) : (
+					<h1>Not Found</h1>
+				)
+			) : (
+				<div>
+					{users.map((user) => (
+						<div>
+							<a
+								key={user.username}
+								href={
+									window.location.protocol +
+									"//" +
+									user.username +
+									"." +
+									window.location.host
+								}
+							>
+								{user.username}
+							</a>
+						</div>
+					))}
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default App;
